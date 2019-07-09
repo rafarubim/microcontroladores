140c2011959047935272c50438c621154f8a1bcf
GabrielManhaes <43486121+GabrielManhaes@users.noreply.github.com>
2019-07-09 17:53:38 -0300

    Update shootingStars.cpp

--- shootingStars/shootingStars.cpp
+++ shootingStars/shootingStars.cpp
@@ -3,33 +3,31 @@
 #include "Graphics.hpp"
 #include <arduino.h>
 #include "Pos.h"
-#include "music.h"
 #include <GFButton.h>
 
-unsigned long previousMillis = 0;
-int current = -1;
-Note shootingstars[] = {{Dsharp5, 1500/2, 375/2},{Dsharp5, 250/2, 250/2},{E5, 500/2, 375/2},{B4, 500/2, 0},{Gsharp4, 250/2, 50/2},
-                 {Dsharp5, 1500/2, 375/2},{Dsharp5, 250/2, 250/2},{E5, 500/2, 375/2},{B4, 500/2, 0},{Gsharp4, 250/2, 50/2},
-                 {Dsharp5, 1500/2, 375/2},{Dsharp5, 250/2, 250/2},{E5, 500/2, 375/2},{B4, 500/2, 0},{Gsharp4, 250/2, 50/2},
-                 {Dsharp5, 1500/2, 375/2},{Dsharp5, 250/2, 250/2},{E5, 500/2, 375/2},{B4, 500/2, 0},{Gsharp4, 250/2, 50/2},
-                 {Dsharp5, 1500/2, 375/2},{Dsharp5, 250/2, 250/2},{E5, 500/2, 375/2},{B4, 500/2, 0},{Gsharp4, 250/2, 50/2},
-                 {Dsharp5, 1500/2, 375/2},{Dsharp5, 250/2, 250/2},{E5, 500/2, 375/2},{B4, 500/2, 0},{Gsharp4, 250/2, 50/2},
-                 {Dsharp5, 1500/2, 375/2},{Dsharp5, 250/2, 250/2},{E5, 500/2, 375/2},{B4, 500/2, 0},{Gsharp4, 250/2, 50/2},
-                 {Dsharp5, 1500/2, 375/2},{Dsharp5, 250/2, 250/2},{E5, 500/2, 375/2},{Dsharp5, 500/2, 0},{B4, 500/2, 0},{Gsharp4, 250/2, 50/2},
-                 {Fsharp5, 1500/2, 375/2},{Fsharp5, 250/2, 250/2},{Gsharp5, 500/2, 375/2},{Dsharp5, 500/2, 0},{B4, 250/2, 50/2},
-                 {Fsharp5, 1500/2, 375/2},{Fsharp5, 250/2, 250/2},{Gsharp5, 500/2, 375/2},{Dsharp5, 500/2, 0},{B4, 250/2, 50/2},
-                 {Fsharp5, 1500/2, 375/2},{Fsharp5, 250/2, 250/2},{Gsharp5, 500/2, 375/2},{Dsharp5, 500/2, 0},{B4, 250/2, 50/2},
-                 {Fsharp5, 1500/2, 375/2},{Fsharp5, 250/2, 250/2},{Gsharp5, 500/2, 375/2},{Dsharp5, 500/2, 0},{B4, 250/2, 50/2},
-                 {Fsharp5, 1500/2, 375/2},{Fsharp5, 250/2, 250/2},{Gsharp5, 500/2, 375/2},{Dsharp5, 500/2, 0},{B4, 250/2, 50/2},
-                 {Fsharp5, 1500/2, 375/2},{Fsharp5, 250/2, 250/2},{Gsharp5, 500/2, 375/2},{Dsharp5, 500/2, 0},{B4, 250/2, 50/2},
-                 {Fsharp5, 1500/2, 375/2},{Fsharp5, 250/2, 250/2},{Gsharp5, 500/2, 375/2},{Dsharp5, 500/2, 0},{B4, 250/2, 50/2},
-                 {Fsharp5, 1500/2, 375/2},{Fsharp5, 250/2, 250/2},{Gsharp5, 500/2, 375/2},{Fsharp5, 500/2, 0},{Dsharp5, 500/2, 0},{B4, 250/2, 50/2}};
-                 
-bool outputTone = false;
-const int tonePin = 6;
-const int ground = A7;
-
 #define WALL_AMOUNT 3
+
+static char* Menu[] = {" ____     _    _      __       __     ____     __     __ _      ___          ____     ____     __      ____     ____ ",
+                       "|*___)   | |  |*|    /  *     /  *   (_ *_)   (  )   |  | |    / __)        /*___)   (_  _)   /*_ |   (  _ |   /*___)",
+                       "|___ |   | )__( |   (  O )   (  O )    )( *    )(    |  * |   ( (__|        |___*|    *)(    | || |    ) * /   |___*|",
+                       "(____|*  |*|  | |    *__/     *__/    (__)    (__)   *_|__|    *___/        (____/    (__)   |_/|_/   (__|_)   (____/"};
+               
+//Note shootingstars[] = {{Dsharp5, 1500/2, 375/2},{Dsharp5, 250/2, 250/2},{E5, 500/2, 375/2},{B4, 500/2, 0},{Gsharp4, 250/2, 50/2},
+//                 {Dsharp5, 1500/2, 375/2},{Dsharp5, 250/2, 250/2},{E5, 500/2, 375/2},{B4, 500/2, 0},{Gsharp4, 250/2, 50/2},
+//                 {Dsharp5, 1500/2, 375/2},{Dsharp5, 250/2, 250/2},{E5, 500/2, 375/2},{B4, 500/2, 0},{Gsharp4, 250/2, 50/2},
+//                 {Dsharp5, 1500/2, 375/2},{Dsharp5, 250/2, 250/2},{E5, 500/2, 375/2},{B4, 500/2, 0},{Gsharp4, 250/2, 50/2},
+//                 {Dsharp5, 1500/2, 375/2},{Dsharp5, 250/2, 250/2},{E5, 500/2, 375/2},{B4, 500/2, 0},{Gsharp4, 250/2, 50/2},
+//                 {Dsharp5, 1500/2, 375/2},{Dsharp5, 250/2, 250/2},{E5, 500/2, 375/2},{B4, 500/2, 0},{Gsharp4, 250/2, 50/2},
+//                 {Dsharp5, 1500/2, 375/2},{Dsharp5, 250/2, 250/2},{E5, 500/2, 375/2},{B4, 500/2, 0},{Gsharp4, 250/2, 50/2},
+//                 {Dsharp5, 1500/2, 375/2},{Dsharp5, 250/2, 250/2},{E5, 500/2, 375/2},{Dsharp5, 500/2, 0},{B4, 500/2, 0},{Gsharp4, 250/2, 50/2},
+//                 {Fsharp5, 1500/2, 375/2},{Fsharp5, 250/2, 250/2},{Gsharp5, 500/2, 375/2},{Dsharp5, 500/2, 0},{B4, 250/2, 50/2},
+//                 {Fsharp5, 1500/2, 375/2},{Fsharp5, 250/2, 250/2},{Gsharp5, 500/2, 375/2},{Dsharp5, 500/2, 0},{B4, 250/2, 50/2},
+//                 {Fsharp5, 1500/2, 375/2},{Fsharp5, 250/2, 250/2},{Gsharp5, 500/2, 375/2},{Dsharp5, 500/2, 0},{B4, 250/2, 50/2},
+//                 {Fsharp5, 1500/2, 375/2},{Fsharp5, 250/2, 250/2},{Gsharp5, 500/2, 375/2},{Dsharp5, 500/2, 0},{B4, 250/2, 50/2},
+//                 {Fsharp5, 1500/2, 375/2},{Fsharp5, 250/2, 250/2},{Gsharp5, 500/2, 375/2},{Dsharp5, 500/2, 0},{B4, 250/2, 50/2},
+//                 {Fsharp5, 1500/2, 375/2},{Fsharp5, 250/2, 250/2},{Gsharp5, 500/2, 375/2},{Dsharp5, 500/2, 0},{B4, 250/2, 50/2},
+//                 {Fsharp5, 1500/2, 375/2},{Fsharp5, 250/2, 250/2},{Gsharp5, 500/2, 375/2},{Dsharp5, 500/2, 0},{B4, 250/2, 50/2},
+//                 {Fsharp5, 1500/2, 375/2},{Fsharp5, 250/2, 250/2},{Gsharp5, 500/2, 375/2},{Fsharp5, 500/2, 0},{Dsharp5, 500/2, 0},{B4, 250/2, 50/2}};
 
 static byte STAR[8] = {
   0b00000,
@@ -41,19 +39,27 @@ static byte STAR[8] = {
   0b10001,
   0b00000
 };
-
-GFButton botao(A8, E_GFBUTTON_PULLDOWN);
-
+const int buttonPin = A1;
+int buttonState = 0;
+bool outputTone = false;
+const int tonePin = 6;
+const int ground = A7;
+unsigned long previousMillis = 0;
+int current = -1;
 static byte WALL = 0xff;
-
-static long level = 0 ; // Store the average value
 static unsigned long score;
-
 static int STARSize = 6;
 static int maxY = 34;
 static int minY = 0 + STARSize;
 static int maxRow = 3;
 static bool playing = false;
+double velocityY = 0;
+double gravity = 80;
+double positionY = 17;
+double positionX = 0;
+static unsigned long time_flag = 0;
+static double moment;
+
 
 struct Wall {
   int x;
@@ -63,6 +69,41 @@ struct Wall {
 
 Wall walls[WALL_AMOUNT] = {Wall(7,random(0,4)), Wall(12, random(0,4)), Wall(17, random(0,4))};
 
+
+void startGame(){
+  Lcd lcd = Lcd::getInstance();
+  lcd.clear();
+  velocityY = 0;
+  positionX = 0;
+  positionY = 17;
+  for ( int scroll = 0; scroll < 49; scroll++) {
+    for( int i = 0; i < 4; i++ ) {
+      for( int j = scroll; j < (20+scroll); j++) {
+        lcd.stamp(Menu[i][j+scroll], j-scroll, i);
+      }
+    }
+    if (scroll == 48) {
+      Animation::opening();
+    }
+    delay(100);
+  }
+  lcd.setCursor(1,1);
+  lcd.print("Pressione o botao");
+  lcd.setCursor(4,2);
+  lcd.print("para jogar");
+  for(;;){
+    buttonState = digitalRead(buttonPin);
+    if (buttonState == HIGH) {
+      playing = !playing;
+      return;
+    }
+    lcd.noDisplay();
+    delay(500);
+    lcd.display();
+    delay(500);
+  }
+}
+
 void drawWall(Lcd* lcd, Wall& wall) {
   for( int y = 0; y < 5; y++ ){
     if ( y != wall.gap ){
@@ -71,17 +112,8 @@ void drawWall(Lcd* lcd, Wall& wall) {
   }
 }
 
-double velocityY = 0;
-double gravity = 80;
-double positionY = 17;
-double positionX = 0;
-static unsigned long time_flag = 0;
-static double moment;
 
-static void start() {
-  if ( !playing )
-    playing = true;
-  
+static void jump() {
   velocityY = -36;
 }
 
@@ -104,59 +136,62 @@ static void Update(float moment, Graphics& graphics) {
 }
 
 static bool checkCollision(Wall walls[]) {
-  for(int i = 0; i < WALL_AMOUNT; i++)
-    if ( ( (int)(positionX / 5) - 1 == (int)walls[i].x )) {
-      Serial.print("posY: ");
-      Serial.println((int)positionY/8);
-      Serial.print("walls: ");
-      Serial.println((int)walls[i].gap + 1);
-      if ( (int)(positionY / 8) != ((int)walls[i].gap + 1) ){
-        return true;
-      }
+  double cX = positionX + 2.5;
+  double cY = positionY - 3.5;
+  double radius = 2;
+  
+  for(int i = 0; i < WALL_AMOUNT; i++){
+
+//    if (((cX + radius) >= 5*(walls[i].x + 1)) && ((cX - radius) <= 5*(walls[i].x + 2)) && ((cY + radius) >= 8*(walls[i].gap + 1)) && ((cY - radius) <= 7*(walls[i].gap))) {
+    if (((cX - radius) <= 5*(walls[i].x + 1)) && ((cX + radius) >= 5*(walls[i].x + 1))) {
+      return true;
     }
+  }
   return false;
 }
 
 void setupGameStar ()
 {
   Serial.begin(9600);
-  botao.setPressHandler(start);
+  pinMode(buttonPin, INPUT);
   pinMode(ground, OUTPUT);
   digitalWrite(ground, LOW);
   pinMode(tonePin, OUTPUT);
+  
 }
    
 void gameLoopStar(Lcd* lcd)
 { 
-  char input = Serial.read();
-  if (input == 'a')
-    start();
-  else if (input == 'b'){
-    playing = !playing;
-  }
+  
   Graphics graphics = Graphics::getInstance();
-  botao.process();
+  
   if ( playing ) {
-    if ( checkCollision(walls) ) {
-      graphics.flushScreen();
-      playing = !playing;
-      velocityY = 0;
-      positionX = 0;
-      positionY = 17;
-      return;
-    }
 //    previousMillis = play(shootingstars, (sizeof(shootingstars)/sizeof(Note)) - 1 , previousMillis, current, outputTone, tonePin);
+    if ( checkCollision(walls) ) {
+        lcd->clear();
+        velocityY = 0;
+        positionX = 0;
+        positionY = 17;
+        playing = !playing;
+        return;
+    }
     moment = (millis() - time_flag)/1000.;
     Update(moment, graphics);
     graphics.draw(STAR, Pos(floor(positionX), floor(positionY)));
+
+    buttonState = digitalRead(buttonPin);
+    if (buttonState == HIGH) {
+      jump();
+    }
     
     for (int i = 0; i < WALL_AMOUNT; i++) {
       drawWall(lcd, walls[i]);
     }
   }
-//  else {
+  else {
+    startGame();
 //    noTone(tonePin);
-//  }
+  }
   time_flag = millis();
   graphics.processGraphics();
 }
